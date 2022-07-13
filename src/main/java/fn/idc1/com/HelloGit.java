caf9fbfec078e152c749cec9dd7cdec0e2e3f3b3
FN\carven.chen <295662385@qq.com>
2022-07-13 23:01:38 +0800

    master添加

--- src/main/java/fn/idc1/com/HelloGit.java
+++ src/main/java/fn/idc1/com/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args){
         System.out.println("1");
         System.out.println("创建dev分支");
+        System.out.println("master添加");
     }
 }
